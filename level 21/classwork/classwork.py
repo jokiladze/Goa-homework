7d11185dcbfef7f90f0b8c2e4afb1f4c69472731
jokiladze <rezi99742@gmail.com>
2025-03-13 12:55:55 +0300

    second commit

---
+++ level 21/classwork/classwork.py
@@ -0,0 +1,21 @@
+#1) შევქმნათ სია (List) ჩვენ საყვარელ მანქანებზე და დავპრინტოთ
+cars = ["Maclren", "BMW", "Audi", "Mercedes", "Porsche"]
+print(cars)
+
+#2) შევქმნათ სია (List) ჩვენ საყვარელ საჭმელზე და 
+#დავპრინტოთ
+foods = ["Pizza", "Sushi", "Pasta", "Burger", "Ice Cream"]
+print(foods)
+
+#3)num = [1,  2,  3,   4,  5,  6,  7,   8,  9,  10] ამ სიაში დაპრინტეთ თითოეული 
+#მათგანი და დაპრინტეთ 2 -იდან 8 ჩათვლით, 5 - იდან 10 ჩათვლით
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for n in num:
+    print(n)
+
+subset1 = num[1:8]
+print(subset1)
+
+subset2 = num[4:10]
+print(subset2)
